9b49405563c415a7cf24c9a4a32955dfc3c87126
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2024-11-11 18:42:07 -0600

    rendered fetched data

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function fetchUserData() {
             }
             return response.json();
         })
-        .then(data => { console.log(data) })
+        .then(data => { displayUsers(data) })
         .catch(error => {
             errorMessage.textContent = error;
             console.error("Error fetching user data:", error);
@@ -21,7 +21,21 @@ function fetchUserData() {
 
 // function to render each user on the page
 function displayUsers(users) {
+    users.forEach((user) => {
+        // create a div for each user
+        const userDiv = document.createElement("div")
+        userDiv.classList.add("user");
 
+        // add user details
+        userDiv.innerHTML = `
+            <h3>${user.name}</h3>
+            <p><strong>Email:</strong> ${user.email}</p>
+            <p><strong>Address:</strong> ${user.address.street}, ${user.address.city}</p>
+        `
+
+        // append the user div to the userList
+        userList.appendChild(userDiv)
+    })
 }
 
 fetchUserData();
